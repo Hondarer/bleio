5e28f245d40f7fdec49cc44d47cc8e6223a037db
Tetsuo Honda <t-honda@hondarer-soft.com>
2025-12-28 21:30:43 +0900

    FLICKER1 の y_normalized 計算が正しく EMA (指数移動平均) を適用していなかった問題を修正

--- bleio-client/Program.cs
+++ bleio-client/Program.cs
@@ -24,8 +24,8 @@ class Program
             await client.SetSerialLedColorAsync(18, 1, 0, 0, 0);
             await client.SetSerialLedColorAsync(18, 2, 0, 0, 0);
             await Task.Delay(1000);
-            await client.SetSerialLedPatternAsync(pin: 18, ledIndex: 1, pattern: BleioClient.SerialLedPattern.Flicker3, param1: 128, param2: 128);
-            await client.SetSerialLedPatternAsync(pin: 18, ledIndex: 2, pattern: BleioClient.SerialLedPattern.Flicker3, param1: 128, param2: 128);
+            await client.SetSerialLedPatternAsync(pin: 18, ledIndex: 1, pattern: BleioClient.SerialLedPattern.Flicker1, param1: 128, param2: 128);
+            await client.SetSerialLedPatternAsync(pin: 18, ledIndex: 2, pattern: BleioClient.SerialLedPattern.Flicker2, param1: 128, param2: 128);
             await client.SetSerialLedColorAsync(18, 1, 255, 182, 75);
             await client.SetSerialLedColorAsync(18, 2, 255, 182, 75);
             return;
